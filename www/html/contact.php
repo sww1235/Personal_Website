3c537cc377d054c40012c2b6ec4169d8f0e19c02
sww1235 <sww1235@gmail.com>
2016-05-16 17:58:33 -0600

    First commit

---
+++ www/html/contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" type="text/css" href="../styles/main.css">
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="icon" href="../images/favicon.ico" type="image/x-icon">
+</head>
+<body>
+<?php include "header.php"; ?>
+
+<?php include "footer.php"; ?>
+</body>
+</html>
---
+++ www/html/header.php
@@ -0,0 +1,22 @@
+<div id="top">
+    <a href="index.php">
+        <img src="../images/logo.png" alt="Home" width="280" height="32">
+    </a>
+
+</div>
+
+<div id="topnavbar">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="stuff.php">Stuff</a>
+            <ul>
+                <li><a href="https://github.com/sww1235">Github</a></li>
+
+            </ul>
+        </li>
+        <li><a href="">Blog</a></li>
+        <li><a href="contact.php">Contact</a></li>
+
+    </ul>
+</div>
