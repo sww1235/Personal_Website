59361d2538bf91a7b346ef54ef953671e6e04314
Stephen Walker-Weinshenker <sww1235@sww1235.net>
2021-05-21 16:23:05 -0600

    adding reference

--- filters/links-to-html.lua
+++ filters/links-to-html.lua
@@ -1,3 +1,5 @@
+-- https://stackoverflow.com/questions/40993488/convert-markdown-links-to-html-with-pandoc
+# links-to-html.lua
 function Link(el)
 	el.target = string.gsub(el.target, "%.md", ".html")
 	return el
