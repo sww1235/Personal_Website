16658dc6a6d4a83d128f0437232177cc7cbef223
Stephen Walker-Weinshenker <sww1235@sww1235.net>
2021-05-23 12:53:45 -0600

    Started auto-generating nav bar

--- build.sh
+++ build.sh
@@ -11,13 +11,14 @@ VERBOSE="--verbose"
 # https://www.arthurkoziel.com/convert-md-to-html-pandoc/
 # use pandoc and HTML template to convert markdown files to HTML.
 
-# iterate through all main directories, run pandoc (maybe with separate option files)
-# and copy to appropriate directory within outdir.
+# First, generate navbar links, then iterate through all main directories, run
+# pandoc (maybe with separate option files) and copy to appropriate directory
+# within outdir.
 
-# makefile isn't useful to only rebuild modified contents, as
-# the output it would be comparing to has lost all meaningful timestamps
-# due to using git. Thus we use a shell script and reduce our dependancies.
-# running in CI doesn't help with this either.
+# makefile isn't useful to only rebuild modified contents, as the output it
+# would be comparing to has lost all meaningful timestamps due to using git.
+# Thus we use a shell script and reduce our dependancies.  running in CI
+# doesn't help with this either.
 
 # want output directory layout as follows:
 # The output directory becomes webroot when processed by travis
@@ -29,6 +30,95 @@ VERBOSE="--verbose"
 # out/wiki/*
 # out/blog-contents/*
 # out/styles/*
+
+# first generate navbar file (included by pandoc)
+
+# Format similar to below:
+#<nav>
+#    <ul>
+#        <li><a href="index.html">Home</a></li>
+#        <li><a href="about.html">About</a></li>
+#        <li><a href="stuff.html">Stuff</a>
+#            <ul>
+#                <li><a href="https://github.com/sww1235">Github</a></li>
+#                <li><a href="wiki/index.html">Wiki</a></li>
+#
+#            </ul>
+#        </li>
+#        <li><a href="blog-contents">Blog</a></li>
+#        <li><a href="contact.html">Contact</a></li>
+#
+#    </ul>
+#</nav>
+echo "starting to build nav-bar"
+navbar="html-parts/nav-bar.html"
+# clear out file
+: > $navbar
+# then create the file anew
+{
+	printf '<nav>\n'
+	printf '\t<ul>\n'
+	printf '\t\t<li><a href=\"index.html\">Home</a></li>\n'
+	printf '\t\t<li><a href=\"about.html\">About</a></li>\n'
+	printf '\t\t<li><a href=\"https://github.com/sww1235\">Github</a></li>\n'
+
+# https://stackoverflow.com/questions/5899337/proper-way-to-make-html-nested-list
+	printf '\t\t<li><a href=\"/projects/index.html\">Projects</a>\n' # don't end li element here
+	printf '\t\t\t<ul>\n' # insert ul element to start sublist instead
+} >> $navbar
+
+echo "starting project loop"
+
+for file in "${PROJDIR}"/*.md; do
+	echo "${file}"
+	basefile=$(basename "$file" ".md")
+	# want to skip index file in the sublist
+	if [ "$basefile" = "index" ]; then
+		continue
+	fi
+	# strip out dashes and upper case first letter of each word
+	# https://unix.stackexchange.com/a/172207/81810
+	title=$(echo "$basefile" | perl -ne 'print join " ", map { ucfirst } split /-/')
+	printf '\t\t\t\t<li><a href=\"%s/%s.html\">%s</a></li>\n' "${PROJDIR}" "$basefile" "${title}" >> $navbar
+done
+{
+	printf '\t\t\t<ul>\n' # end ul element here
+	printf '\t\t<li>\n' # end li element here instead
+
+	# https://stackoverflow.com/questions/5899337/proper-way-to-make-html-nested-list
+	printf '\t\t<li><a href=\"/wiki/index.html\">Wiki</a>\n' >> $navbar # don't end li element here
+	printf '\t\t\t<ul>\n' >> $navbar # insert ul element to start sublist instead
+} >> $navbar
+
+echo "starting wiki loop"
+
+for file in "${WIKIDIR}"/*.md; do
+	echo "${file}"
+	basefile=$(basename "$file" ".md")
+	# want to skip index file in the sublist
+	if [ "$basefile" = "index" ]; then
+		continue
+	fi
+	# strip out dashes and upper case first letter of each word
+	# https://unix.stackexchange.com/a/172207/81810
+	title=$(echo "$basefile" | perl -ne 'print join " ", map { ucfirst } split /-/')
+	printf '\t\t\t\t<li><a href=\"%s/%s.html\">%s</a></li>\n' "${WIKIDIR}" "$basefile" "${title}" >> $navbar
+done
+{
+	printf '\t\t\t<ul>\n' # end ul element here
+	printf '\t\t<li>\n' # end li element here instead
+} >> $navbar
+
+# TODO: how to handle blog contents? One long string of articles or individual pages?
+
+{
+	printf '\t\t<li><a href=\"contact.html\">Contact</a></li>\n'
+
+	printf '\t</ul>\n'
+	printf '</nav>\n'
+} >> $navbar
+
+echo "past nav-bar building"
 
 mkdir -p ${OUTDIR}
 mkdir -p ${OUTDIR}/styles
